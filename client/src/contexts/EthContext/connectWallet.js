9d4c94249419caba4eb166cef200811e84dbc361
joubijuba <juba_ramdani@hotmail.com>
2022-12-09 12:16:27 +0100

    wallet context updated with new contracts

--- client/src/contexts/EthContext/connectWallet.js
+++ client/src/contexts/EthContext/connectWallet.js
@@ -1,21 +1,33 @@
 import Web3 from "web3";
 import { actions } from "./state";
 import eventArtifact from "../../contracts/Event.json"
+import tokenArtifact from "../../contracts/TicketToken.json"
+import icoArtifact from "../../contracts/ICO.json"
+import singleStakingArtifact from "../../contracts/SingleStaking.json"
 
 const connectWallet = async (dispatch) => {
         const web3 = new Web3(Web3.givenProvider || "ws://localhost:8545");
         const accounts = await web3.eth.requestAccounts();
         const networkID = await web3.eth.net.getId();
         let eventAddress, eventContract;
+        let tokenAddress, tokenContract;
+        let icoAddress, icoContract;
+        let singleStakingAddress, singleStakingContract;
         try {
           eventAddress = eventArtifact.networks[networkID].address;
           eventContract = new web3.eth.Contract(eventArtifact.abi, eventAddress);
+          tokenAddress = tokenArtifact.networks[networkID].address;
+          tokenContract = new Web3.eth.Contract(tokenArtifact.abi, tokenAddress);
+          icoAddress = icoArtifact.networks[networkID].address ;
+          icoContract = new Web3.eth.Contract(icoArtifact.abi, icoAddress);
+          singleStakingAddress = icoArtifact.networks[networkID].address ;
+          singleStakingContract = new Web3.eth.Contract(singleStakingArtifact.abi, singleStakingAddress) ;
         } catch (err) {
           console.error(err);
         }
         dispatch({
           type: actions.init,
-          data: { eventArtifact, web3, accounts, networkID, eventContract }
+          data: { eventArtifact, web3, accounts, networkID, eventContract, tokenContract, icoContract, singleStakingContract }
         });
   
 };
