af7f0f3c1581d69c26c56d1f892cc4770bb9fcae
Badisse Bouabdallah <badisse.bouabdallah@proton.me>
2022-12-09 06:43:49 +0100

    fix: routes

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import { EthProvider } from "./contexts/EthContext";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Create from "./pages/Create";
+import Explore from "./pages/Explore";
+import NFTDetail from "./pages/NFTDetail";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <EthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/create" element={<Create />} />
+          <Route path="/explore" element={<Explore />} />
+          <Route path="/detail" element={<NFTDetail />} />
+        </Routes>
+      </BrowserRouter>,
+    </EthProvider>
   </React.StrictMode>
 );
